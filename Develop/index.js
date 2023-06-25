04f002ede75a8b5516247929eb30cfce6ba53952
RubenRuiz411 <rubenmruiz.411@gmail.com>
2023-06-24 21:50:40 -0700

    completed assignment with no video no readme no video

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,6 @@
-// TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs")
-
-// TODO: Create an array of questions for user input
+const genMarkdown = require("./utils/generateMarkdown.js")
 const questions = [
     {
         type: 'input',
@@ -36,83 +34,24 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'credts',
+        name: 'credits',
         message: "Credits:",
     },
     {
-        type: 'input',
-        name: 'badges',
-        message: "Badge:",
+        type: 'list',
+        name: 'license',
+        message: "What license would you like?",
+        choices: ["MIT", "AGP", "GPL", "none",]
     },
-
 ];
 
-
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    inquirer.prompt(questions)
-    .then((data) => {
-        var readMe = `
-        # README
-
-## Description
-
-${data.description}
-
-- What was your motivation?
-
-${data.motivation}
-
-- Why did you build this project? 
-
-${data.project}
-
-- What problem does it solve?
-
-${data.solve}
-
-- What did you learn?
-
-${data.learn}
-
-## Installation
-
-N/A
-
-## Usage
-
-${data.usage}
-
-## Credits
-
-${data.credits}
-
-## License
-
-Copyright 2021 Ruben Ruiz 
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:  
-    
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. 
-    
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
-
-
-
-## Badges
-
-${data.badges}`
-
+inquirer.prompt(questions)
+.then((data) => {
+const readMe = genMarkdown(data);
 fs.writeFile('README.md', readMe, (err) =>
 err ? console.log(err) : console.log('success!'))
-    })
+});
 
-    
-}
 
-// TODO: Create a function to initialize app
 function init() {}
-
-// Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,60 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license=== "none" ){
+    return ""    
+  } else {
+    return `![license](https://img.shields.io/badge/license-${license}-green)`
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  var readMe = `
+# README ${renderLicenseBadge(data.license)}
 
-`;
+## Description
+
+${data.description}
+
+- What was your motivation?
+
+${data.motivation}
+
+- Why did you build this project? 
+
+${data.project}
+
+- What problem does it solve?
+
+${data.solve}
+
+- What did you learn?
+
+${data.learn}
+
+## Installation
+
+N/A
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+${data.credits}
+
+${data.license !== "none" ? `## License
+
+Copyright 2021 Ruben Ruiz 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:  
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. 
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
+`:``}
+`
+
+return readMe
 }
 
 module.exports = generateMarkdown;
